443033cb4f091e8c85183a196b013eb13abb6c32
sanket <sanknavy@gmail.com>
2018-11-15 13:12:43 +0530

    on order create add tracing no to note

---
+++ app/helpers/order_tracking_helper.rb
@@ -0,0 +1,2 @@
+module OrderTrackingHelper
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'order_tracking#home'
+  post 'order_tracking/order_webhook'
+  # match 'order_tracking/order_webhook', to: 'order_tracking#order_webhook', via: :all
+end
